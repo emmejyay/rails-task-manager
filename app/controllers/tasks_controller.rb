42a2e7b6d3c2f67e8f6aae62ca4a10005680bba8
Meghan Johnson <meghan.h.johnson@gmail.com>
2022-05-17 15:36:55 +0200

    completed functionality of rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,38 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params[:id])
+    @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  # form items are not allowed by default unless required using the function below
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
